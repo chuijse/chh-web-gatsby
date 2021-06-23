42b6b3849666fc87c041f57bc2d37338372cbc13
Cristian Huijse <cristian.huijse@gmail.com>
2021-06-22 21:48:45 -0400

    animate picker and Cards on Teaching

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -1,22 +1,46 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 export function Picker(props) {
   const [open, setOpen] = useState(false);
+  const [animation, setAnimation] = useState(false);
+
+  function handleChange() {
+    if (animation === false) {
+      setAnimation(!animation);
+      setOpen(!open);
+    } else {
+      setAnimation(false);
+      setTimeout(() => {
+        setOpen(false);
+      }, 500);
+    }
+  }
 
   return (
     <React.Fragment>
-      <div onClick={() => setOpen(!open)}>
+      <div onClick={() => handleChange()}>
         <div className="picker">
           <motion.div
             className="triangle"
-            animate={{ rotate: open ? 0 : -90 }}
+            animate={{ rotate: animation ? 0 : -90 }}
           />
           <h2>{props.filter}</h2>
         </div>
-        <nav className="picker-nav">
-          <ul className="picker-ul">{open && props.children}</ul>
-        </nav>
+
+        <motion.nav
+          className="picker-nav"
+          animate={{ opacity: animation ? 1 : 0, y: animation ? 0 : -60 }}
+          exit={{ opacity: animation ? 1 : 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <motion.ul
+            animate={{ opacity: animation ? 1 : 0 }}
+            className="picker-ul"
+          >
+            {open && props.children}
+          </motion.ul>
+        </motion.nav>
       </div>
     </React.Fragment>
   );
@@ -24,12 +48,16 @@ export function Picker(props) {
 
 export function PickerItem(props) {
   return (
-    <li
-      onClick={() => {
-        props.onSelect(props.name);
-      }}
-    >
-      {props.name}
-    </li>
+    <React.Fragment>
+      {props.name === props.filter ? null : (
+        <li
+          onClick={() => {
+            props.onSelect(props.name);
+          }}
+        >
+          {props.name}
+        </li>
+      )}
+    </React.Fragment>
   );
 }
--- src/pages/teaching.js
+++ src/pages/teaching.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, graphql } from "gatsby";
 import Card from "../components/Card";
 import { Picker, PickerItem } from "../components/Picker";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Teaching({ data }) {
   const [filter, setCategory] = useState("All");
@@ -14,14 +15,30 @@ export default function Teaching({ data }) {
         <div className="intro">
           <span className="centerText">
             <h1>Mi carrera docente</h1>
-            <div>
+            <div className="subHeader">
               <h2>Filtra por Universidad</h2>
 
               <Picker filter={filter}>
-                <PickerItem name={"All"} onSelect={setCategory} />
-                <PickerItem name={"UDD"} onSelect={setCategory} />
-                <PickerItem name={"UDP"} onSelect={setCategory} />
-                <PickerItem name={"UACH"} onSelect={setCategory} />
+                <PickerItem
+                  name={"All"}
+                  onSelect={setCategory}
+                  filter={filter}
+                />
+                <PickerItem
+                  name={"UDD"}
+                  onSelect={setCategory}
+                  filter={filter}
+                />
+                <PickerItem
+                  name={"UDP"}
+                  onSelect={setCategory}
+                  filter={filter}
+                />
+                <PickerItem
+                  name={"UACH"}
+                  onSelect={setCategory}
+                  filter={filter}
+                />
               </Picker>
             </div>
           </span>
@@ -31,36 +48,47 @@ export default function Teaching({ data }) {
           <div>Loading...</div>
         ) : (
           <div className="teachingGrid">
-            {courses
-              .filter((course) =>
-                course.universities.acronym.includes(
-                  filter === "All" ? "" : filter
+            <AnimatePresence exitBeforeEnter>
+              {courses
+                .filter((course) =>
+                  course.universities.acronym.includes(
+                    filter === "All" ? "" : filter
+                  )
                 )
-              )
-              .map((filteredCourse) => (
-                <span key={filteredCourse.id}>
-                  <Link to={`/teaching/${filteredCourse.slug.current}`}>
-                    <Card
-                      color={
-                        filteredCourse.universities.acronym === "UDD"
-                          ? "card_UDD"
-                          : filteredCourse.universities.acronym === "UDP"
-                          ? "card_UDP"
-                          : filteredCourse.universities.acronym === "UACH"
-                          ? "card_UACH"
-                          : null
-                      }
-                      title={filteredCourse.title}
-                      mainImage={filteredCourse.mainImage.asset.gatsbyImageData}
-                      mainCaption={filteredCourse.mainImage.caption}
-                      university={filteredCourse.universities.acronym}
-                      abstract={filteredCourse.abstract}
-                      year={filteredCourse.year}
-                      semester={filteredCourse.semester}
-                    ></Card>
-                  </Link>
-                </span>
-              ))}
+                .map((filteredCourse) => (
+                  <span key={filteredCourse.id}>
+                    <Link to={`/teaching/${filteredCourse.slug.current}`}>
+                      <motion.div
+                        initial={{ opacity: 0, y: 100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: 100 }}
+                        transition={{ duration: 1 }}
+                      >
+                        <Card
+                          color={
+                            filteredCourse.universities.acronym === "UDD"
+                              ? "card_UDD"
+                              : filteredCourse.universities.acronym === "UDP"
+                              ? "card_UDP"
+                              : filteredCourse.universities.acronym === "UACH"
+                              ? "card_UACH"
+                              : null
+                          }
+                          title={filteredCourse.title}
+                          mainImage={
+                            filteredCourse.mainImage.asset.gatsbyImageData
+                          }
+                          mainCaption={filteredCourse.mainImage.caption}
+                          university={filteredCourse.universities.acronym}
+                          abstract={filteredCourse.abstract}
+                          year={filteredCourse.year}
+                          semester={filteredCourse.semester}
+                        ></Card>
+                      </motion.div>
+                    </Link>
+                  </span>
+                ))}
+            </AnimatePresence>
           </div>
         )}
       </div>
@@ -70,7 +98,7 @@ export default function Teaching({ data }) {
 
 export const query = graphql`
   query TeachingPage {
-    allSanityCourses {
+    allSanityCourses(sort: { fields: [year], order: DESC }) {
       nodes {
         title
         id
